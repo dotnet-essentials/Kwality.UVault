09823b69e5fb298cede2c7cc7d56875259b1f251
Kevin De Coninck <kevin.dconinck@gmail.com>
2023-05-11 15:25:13 +0200

    Add: `ComVisible` attribute
    - Add `ComVisible` attribute to the assemblies.

--- app/Kwality.UVault/Properties/AssemblyInfo.cs
+++ app/Kwality.UVault/Properties/AssemblyInfo.cs
@@ -22,8 +22,6 @@
 // =                FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // =                OTHER DEALINGS IN THE SOFTWARE.
 // =====================================================================================================================
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: InternalsVisibleTo("Kwality.UVault.QA")]
