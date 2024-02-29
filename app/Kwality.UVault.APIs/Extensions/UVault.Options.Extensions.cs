d438eb3c8b70d710b18bdbba4bfdbbf3e308501b
Kevin De Coninck <kevin.dconinck@gmail.com>
2024-02-29 07:40:04 +0100

    Fix: SonarCloud warnings
    Fix the last sonarcloud warnings.

--- app/Kwality.UVault.APIs/Extensions/UVault.Options.Extensions.cs
+++ app/Kwality.UVault.APIs/Extensions/UVault.Options.Extensions.cs
@@ -38,7 +38,9 @@ using Microsoft.Extensions.DependencyInjection;
 [PublicAPI]
 public static class UVaultOptionsExtensions
 {
+#pragma warning disable S4018
     public static void UseApiManagement<TModel, TKey>(this UVaultOptions options)
+#pragma warning restore
         where TModel : ApiModel<TKey>
         where TKey : IEqualityComparer<TKey>
     {
